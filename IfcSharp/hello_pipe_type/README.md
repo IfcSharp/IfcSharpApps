36e1df9564f02b052d4ae41171601dd78be1ef85
Bernhard Simon Bock <bsbock@bock-nw.de>
2020-10-24 18:33:09 +0200

    remove pipe_type

--- IfcSharp/hello_pipe_type/README.md
+++
@@ -1,8 +0,0 @@
-﻿<!-- IfcSharp-documentation, Copyright (c) 2020, Bernhard Simon Bock, Friedrich Eder, MIT License (see https://github.com/IfcSharp/IfcSharpLibrary/tree/master/Licence) --->
-
-# hello_pipe_type
-
-hello_pipe_type is en example of using multiple instances of a producttype.
-
-
-
--- IfcSharp/hello_schema/hello_schema.cs
+++ IfcSharp/hello_schema/hello_schema.cs
@@ -1,17 +1,26 @@
-﻿// hello_schema.cs, this software use IfcSharp (see https://github.com/IfcSharp)
+﻿
+// hello_schema.cs, this software use IfcSharp (see https://github.com/IfcSharp)
+using ifc;
+using System.Collections.Generic;
+
 class hello_schema {static void Main(string[] args){//#####################################################################
 try{ 
 
+System.Collections.Generic.SortedSet<string> set=new SortedSet<string>();
+
 ifc.ENTITY.TypeDictionary.FillEntityTypeComponentsDict();
 int Counter=0;
-foreach (ifc.ENTITY.ComponentsType ct in ifc.ENTITY.TypeDictionary.EntityTypeComponentsList) if (++Counter<5)
-        {System.Console.WriteLine("Entity "+ct.EntityType.Name);
+foreach (ifc.ENTITY.ComponentsType ct in ifc.ENTITY.TypeDictionary.EntityTypeComponentsList) //if (++Counter<5)
+        {//System.Console.WriteLine("Entity "+ct.EntityType.Name);
          foreach (System.Reflection.FieldInfo fi in ct.AttribList) 
-                 {foreach (System.Attribute attr in fi.GetCustomAttributes(true)) if (attr is ifcAttribute) System.Console.Write(" ("+((ifcAttribute)attr).OrdinalPosition+")");
-                  System.Console.WriteLine(" "+fi.Name+" "+fi.FieldType.ToString());
+                 {foreach (System.Attribute attr in fi.GetCustomAttributes(true)) if (attr is ifcAttribute) 
+                    if (fi.Name[0]=='_') set.Add(fi.Name);
+                  //System.Console.WriteLine(" "+fi.Name+" "+fi.FieldType.ToString());
                  }
        }
 
+foreach (string s in set) 
+System.Console.WriteLine(s);
+
 }catch(System.Exception e){System.Console.WriteLine(e.Message);} 
 }}//########################################################################################################################
-

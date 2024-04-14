caf0d4883ff66768ca6568f946a2b49d1f3501fc
Bernhard Simon Bock <bsbock@bock-nw.de>
2024-04-14 20:31:54 +0200

    IFC4X3_ADD2

--- IfcSql/ifcStep2ifcSQL/build_all_specifications.bat
+++ IfcSql/ifcStep2ifcSQL/build_all_specifications.bat
@@ -1,9 +1,7 @@
-
 CALL build_specification IFC2X3
 CALL build_specification IFC4
 CALL build_specification IFC4X1
 CALL build_specification IFC4X2
-CALL build_specification IFC4X3_RC1
-CALL build_specification IFC4X3_RC2
+CALL build_specification IFC4X3_ADD2
 pause
 

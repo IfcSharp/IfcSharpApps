751b176b52a4edbef2464beb33592a4947cfebbb
Bernhard Simon Bock <bsbock@bock-nw.de>
2024-11-10 19:15:29 +0100

    bugfixes for ifcSQL interface

--- IfcSql/from_ifcSQL/from_ifcSQL.cs
+++ IfcSql/from_ifcSQL/from_ifcSQL.cs
@@ -1,6 +1,7 @@
 ﻿// from_ifcSQL.cs, this software use IfcSharp (see https://github.com/IfcSharp)
 class from_ifcSQL {static void Main(string[] args){//#######################################################################
 //try{ 
+ifc.Log.Reset(); // bb 10.11.2024
 ifc.ENTITY.IfcLineConstText="= IFC";
 ifc.Model m = ifc.Model.FromSql(ServerName:System.Environment.GetEnvironmentVariable("SqlServer"), DatabaseName:"ifcSQL",ProjectId:0);// 0 = current project
                        m.Header.Name="from_ifcSQL";
---
+++ IfcSql/ifcStep2ifcSQL/IFC4X3_ADD2/test.bat
@@ -0,0 +1,2 @@
+ifcStep2ifcSQL_IFC4X3_ADD2.exe F:\_tmp\20241028_Giorgia\1A.ifc
+pause
